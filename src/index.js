4765cb6de23d4d992023de82b2f36328fa2bfe1b
ashu0909 <greatnatural913@gmail.com>
2018-07-20 18:17:45 +0530

    index.js modified

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ class App extends Component {
             <div>
                 <h2>Hello World!</h2>
                 <Tooltip/>
-                <Button onClick={} type="link"/>
+                <Button type="link" href="http://google.com"/>
             </div>
         )
     }
