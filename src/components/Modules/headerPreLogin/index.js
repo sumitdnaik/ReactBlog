bd5154c46107b2738618d09afa7c9763a923f6fa
Sumit Naik <sumit.naik@globant.com>
2018-07-30 09:57:34 +0530

    Pre login Header

--- src/components/Modules/headerPreLogin/index.js
+++ src/components/Modules/headerPreLogin/index.js
@@ -4,25 +4,19 @@ import constants from '../../../constants/global';
 import Button from '../../elements/button';
 import './style.scss';
 
-class HeaderPreLogin extends Component{
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-      return(
-        <header role='banner'>
-          <div className="header-wrapper">
-            <div className="width-container">
-              <p className="logo"><a href="javascript: void(0)">{constants.logo}</a></p>
-              <div>
-                <Button type="link" href="javascript: void(0)">Sign Up</Button>
-              </div>
+function HeaderPreLogin() {
+    return(
+      <header role='banner'>
+        <div className="header-wrapper">
+          <div className="width-container">
+            <p className="logo"><a href="javascript: void(0)">{constants.logo}</a></p>
+            <div className="right-wrapper">
+              <Button type="link" href="javascript: void(0)">Sign Up</Button>
             </div>
           </div>
-        </header>
-      );
-    }
+        </div>
+      </header>
+    );
 }
 
 export default HeaderPreLogin;
