52cdbcd1ec057dde70d202e95fd42f489ea10cce
Sumit Naik <sumitd.naik6@gmail.com>
2018-10-28 16:09:23 +0530

    Updated URLs as per new backend routes

--- src/constants/APIUrls.js
+++ src/constants/APIUrls.js
@@ -1,12 +1,16 @@
 const APIUrls = {
-  login: 'http://127.0.0.1:8000/api/login/',
-  signUp: 'http://127.0.0.1:8000/api/signup',
-  createStory: 'http://127.0.0.1:8000/api/createStory',
-  readStory: 'http://127.0.0.1:8000/api/readStory',
-  upvoteStory: 'http://127.0.0.1:8000/api/upvoteStory',
-  getHomeStories: 'http://127.0.0.1:8000/api/getHomeStories',
-  saveProfile:'http://127.0.0.1:8000/api/saveProfile',
-  getProfile:'http://127.0.0.1:8000/api/getProfile',
+
+  login: 'http://127.0.0.1:8000/auth/login/',
+  signUp: 'http://127.0.0.1:8000/auth/signup',
+
+  createStory: 'http://127.0.0.1:8000/story/createStory',
+  readStory: 'http://127.0.0.1:8000/story/readStory',
+  upvoteStory: 'http://127.0.0.1:8000/story/upvoteStory',
+  getHomeStories: 'http://127.0.0.1:8000/story/getHomeStories',
+
+  saveProfile:'http://127.0.0.1:8000/user/saveProfile',
+  getProfile:'http://127.0.0.1:8000/user/getProfile',
+  
 }
 
 export default APIUrls;
